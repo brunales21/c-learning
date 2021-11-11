3acba956834d0b5b178c7e6691ea5c0e6ed2ddab
echarly76 <correomercado@hotmail.com>
2021-11-11 22:53:38 +0100

    Create carlos.c

---
+++ carlos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+static void _showNumber(int number, int base) {
+    if (number >= base) {
+        _showNumber(number/base, base);
+        number = number%base;
+    }
+    printf("%c", number < 10 ? 48 + number : 55 + number);
+}
+
+void showNumber(int number, int base) {
+    _showNumber(number, base);
+    printf("\n");
+}
+
+int mainShowNumber() {
+    showNumber(360, 10);
+    showNumber(360, 2);
+    showNumber(360, 16);
+    showNumber(360, 8);
+    
+    return 0;
+}
